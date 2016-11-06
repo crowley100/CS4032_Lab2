18bb2afd50d628a47c2971a716da4df161c2b6f5
Lorcan <loreidy@tcd.ie>
2016-11-06 18:17:22 +0000

    client modified from solution to lab 1

---
+++ client.hs
@@ -0,0 +1,25 @@
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import qualified Data.ByteString.Char8 as B8
+import System.IO
+import System.Posix.Unistd
+
+client :: String -> Int -> IO ()
+client host port = withSocketsDo $ do
+                addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
+                let serverAddr = head addrInfo
+                sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+                connect sock (addrAddress serverAddr)
+                msgSender sock
+
+msgSender :: Socket -> IO ()
+msgSender sock = do
+  handle <- socketToHandle sock ReadWriteMode
+  hSetBuffering handle LineBuffering
+  hPutStrLn handle "GET /server.php?message=its+high+noon HTTP/1.0\r\n\r\n"
+  rMsg <- hGetContents handle
+  putStrLn rMsg
+  hClose handle
+  
+main :: IO ()
+main = client "localhost" 8000
