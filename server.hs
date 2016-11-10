e8a5fa6487689620ceead887ead7eabbc9c74a4d
Lorcan <loreidy@tcd.ie>
2016-11-10 19:19:13 +0000

    implemented thread pooling

--- server.hs
+++ server.hs
@@ -3,30 +3,31 @@ module Main where
 import System.IO
 import Network.Socket
 import Control.Concurrent
+import Control.ThreadPool
 
 -- init
 main :: IO ()
 main = do
-    sock <- socket AF_INET Stream 0    -- new socket
-    setSocketOption sock ReuseAddr 1   -- make socket reuseable
+    sock <- socket AF_INET Stream 0            -- new socket
+    setSocketOption sock ReuseAddr 1           -- make socket reuseable
     bind sock (SockAddrInet 8000 iNADDR_ANY)   -- listen on port 8000.
-    listen sock 3                              -- 2 queued connections
-    mainLoop sock
+    listen sock 3                             -- up to 10 queued connections
+    (input,output) <- threadPoolIO 5 hdlConn
+    mainLoop sock input
   
 -- handle connections  
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
+mainLoop :: Socket -> Chan (Socket, SockAddr) -> IO ()
+mainLoop sock input = do
     conn <- accept sock     -- accept a new client connection
-    forkIO (hdlConn conn)   -- run our server's logic for each client connection in separate thread
-    runConn conn
-    mainLoop sock           -- loop
+    writeChan input conn
+    mainLoop sock input          -- loop
 
 -- server logic
 hdlConn :: (Socket, SockAddr) -> IO ()
 hdlConn (sock, _) = do
     handle <- socketToHandle sock ReadWriteMode
-    hSetBuffering handle LineBuffering -- can change to LineBuffering
+    hSetBuffering handle NoBuffering -- can change to LineBuffering
     hPutStrLn handle "Hello, world!"
     threadDelay 10000000
-    hPutStrLn handle "still there?"
+    hPutStrLn handle "are you still there?"
     hClose handle
